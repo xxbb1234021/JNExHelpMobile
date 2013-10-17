3dec54d95841b128c407f0b4d2f10ef5cefa16e3
xxbb1234021 <xxbb1234021@qq.com>
2013-10-17 17:08:41 +0800

    动画首页

---
+++ src/com/jnexhelp/WelcomeActivity.java
@@ -0,0 +1,76 @@
+package com.jnexhelp;
+
+import com.jnexhelp.Activity.MainActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class WelcomeActivity extends Activity implements AnimationListener
+{
+	private ImageView imageView = null;
+	private Animation animation = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+		
+		imageView = (ImageView) findViewById(R.id.welcome_image_view);
+		animation = AnimationUtils.loadAnimation(this, R.anim.welcome_alpha);
+		animation.setFillAfter(true);//启动Fill保持
+		animation.setFillAfter(true); //设置动画的最后一帧是保持在View上面
+		imageView.setAnimation(animation);
+		animation.setAnimationListener(this); //为动画设置监听
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.welcome, menu);
+		return true;
+	}
+
+	@Override
+	public void onAnimationEnd(Animation animation)
+	{
+		//动画结束时结束欢迎界面并转到软件的主界面
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+		this.finish();
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation animation)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onAnimationStart(Animation animation)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)
+	{
+		//在欢迎界面屏蔽BACK键
+		if (keyCode == KeyEvent.KEYCODE_BACK)
+		{
+			return false;
+		}
+		return false;
+	}
+}
---
+++ src/com/jnexhelp/util/ImagesTextUtil.java
@@ -0,0 +1,12 @@
+package com.jnexhelp.util;
+
+import com.jnexhelp.R;
+
+public class ImagesTextUtil
+{
+	public static int[] mainActivityImages = { R.drawable.ic1, R.drawable.ic2, R.drawable.ic3, R.drawable.ic4, R.drawable.ic5, R.drawable.ic6,
+			R.drawable.ic7, R.drawable.ic8, R.drawable.ic9 };
+
+	public static int[] mainActivityImagesText = { R.string.ic1_text, R.string.ic2_text, R.string.ic3_text, R.string.ic4_text, R.string.ic5_text,
+			R.string.ic6_text, R.string.ic7_text, R.string.ic8_text, R.string.ic9_text };
+}
